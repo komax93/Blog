6ab5994654e9b70125151d683858fe1575bf4242
Maxym Kolesnykov <maxkolesnikov93@gmail.com>
2017-04-23 15:19:06 +0300

    added categories CRUD

--- routes/web.php
+++ routes/web.php
@@ -11,22 +11,30 @@
 |
 */
 
+// Login
 Route::get('auth/login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('auth/login', 'Auth\LoginController@login');
 Route::get('auth/logout', 'Auth\LoginController@logout')->name('logout');
 
+// Register
 Route::get('auth/register', 'Auth\RegisterController@showRegistrationForm')->name('register');
 Route::post('auth/register', 'Auth\RegisterController@register');
 
+// Password Reset Routes
 Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm');
 Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
 Route::get('password/reset/{token?}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
-Route::get('blog/{slug}', ['uses' => 'BlogController@getSingle', 'as' => 'blog.single'])
-    ->where('slug', '[\w\d\-\_]+');
+//Categories
+Route::resource('categories', 'CategoryController');
+
+//Site
+Route::get('blog/{slug}', ['uses' => 'BlogController@getSingle', 'as' => 'blog.single'])->where('slug', '[\w\d\-\_]+');
 Route::get('blog', ['uses' => 'BlogController@getIndex', 'as' => 'blog.index']);
-Route::get('/contact', 'PagesController@getContact');
-Route::get('/about', 'PagesController@getAbout');
 Route::get('/', 'PagesController@getIndex');
 Route::resource('posts', 'PostController');
+
+//Static pages
+Route::get('/contact', 'PagesController@getContact');
+Route::get('/about', 'PagesController@getAbout');
